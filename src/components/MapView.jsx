d47b09b27ba5adda47dc504e9d41ebb72b106074
Aqeel98 <sidequestsrilanka@gmail.com>
2025-12-17 01:34:25 +0530

    Map updates

--- src/components/MapView.jsx
+++ src/components/MapView.jsx
@@ -82,8 +82,9 @@ export function MapView({ quests, questProgress, currentUser, onSelectQuest, set
         scrollWheelZoom
       >
         <TileLayer
-           attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a> &copy; <a href="https://stamen.com/">Stamen Design</a>'
-           url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
+           // Switched to a reliable, free dark tile from CARTO
+           url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png" 
+           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
         />
 
         {/* Auto-Zoom when location is found */}
