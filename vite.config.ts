b5a034d90912b5a5d9f37ca645b4f17520bc5139
sidequestsrilanka@gmail.com <sidequestsrilanka@gmail.com>
2026-01-26 12:16:01 +0530

    force update pwa icons using rename strategy

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: false, 
+      includeAssets: ['apple-touch-icon-v3.png', 'pwa-192-v3.png', 'pwa-512-v3.png'],
+    })
+  ],
   optimizeDeps: {
     include: ['leaflet']
-  }
-})
+  },
+});
