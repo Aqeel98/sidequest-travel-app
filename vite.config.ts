f2b1491e0282de6972da1fd874d7a709600f173f
Aqeel98 <sidequestsrilanka@gmail.com>
2026-02-01 16:47:18 +0530

    Index update

--- vite.config.ts
+++ vite.config.ts
@@ -36,6 +36,11 @@ export default defineConfig({
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any'
+          },
+          {
+            src: 'apple-touch-icon.png',
+            sizes: '180x180',
+            type: 'image/png'
           }
         ]
       },
