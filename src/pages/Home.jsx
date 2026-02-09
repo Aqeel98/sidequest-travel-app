ddb6814dac560f10a335ddf36587c222a970bd38
Aqeel98 <sidequestsrilanka@gmail.com>
2026-02-09 17:08:45 +0530

    Home up

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,8 @@ import { useNavigate } from 'react-router-dom';
 import { 
   MapPin, ArrowRight, Sparkles, PlusCircle, Compass, Mountain, Anchor, 
   Leaf, Waves, Heart, Bird, Palmtree, Backpack, Map, Zap, Ship, 
-  Globe, Trees, Tent, Camera, Sun, Moon, Cat, PawPrint, Gem, Fish, Shell, Feather, Wind 
+  Globe, Trees, Tent, Camera, Sun, Moon, 
+  Cat, PawPrint, Gem, Fish, Shell, Feather, Wind 
 } from 'lucide-react';
 import { useSideQuest } from '../context/SideQuestContext';
 
